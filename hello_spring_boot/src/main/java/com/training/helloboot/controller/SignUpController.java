2f20e002b8cd73ab0b3647a6546730cbce3b715e
Hein Thant <heinthant098@gmail.com>
2022-07-10 10:25:39 +0630

    ｺﾝｽﾄﾗｯｸﾀｲﾝｼﾞｪｸｼｮﾝに変更

--- hello_spring_boot/src/main/java/com/training/helloboot/controller/SignUpController.java
+++ hello_spring_boot/src/main/java/com/training/helloboot/controller/SignUpController.java
@@ -3,7 +3,6 @@ package com.training.helloboot.controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,39 +17,40 @@ import com.training.helloboot.repo.AccountRepo;
 @Controller
 @RequestMapping("/sign-up")
 public class SignUpController {
-	@Autowired
+
 	private BCryptPasswordEncoder passwordEncoder;
-	
-	@Autowired
+
 	private AccountRepo repo;
-	
+
+	public SignUpController(BCryptPasswordEncoder passwordEncoder, AccountRepo repo) {
+		super();
+		this.passwordEncoder = passwordEncoder;
+		this.repo = repo;
+	}
+
 	@GetMapping
 	public String index() {
 		return "/sign-up";
 	}
-	
+
 	@PostMapping
-	public String signUp(
-			@RequestParam String name, 
-			@RequestParam String email, 
-			@RequestParam String password,
+	public String signUp(@RequestParam String name, @RequestParam String email, @RequestParam String password,
 			HttpServletRequest req) {
-			
-			try {
-				Account acc = new Account();
-				acc.setName(name);
-				acc.setEmail(email);
-				acc.setPassword(passwordEncoder.encode(password));
-				acc.setRole(Role.Student);
-				
-				repo.save(acc);
-				
-				req.login(email, password);
-			} catch (ServletException e) {
-				e.printStackTrace();
-			}
-			
-		
+
+		try {
+			Account acc = new Account();
+			acc.setName(name);
+			acc.setEmail(email);
+			acc.setPassword(passwordEncoder.encode(password));
+			acc.setRole(Role.Student);
+
+			repo.save(acc);
+
+			req.login(email, password);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+
 		return "redirect:/home";
 	}
 }
--- hello_spring_boot/src/main/java/com/training/helloboot/service/AccountUserDetailService.java
+++ hello_spring_boot/src/main/java/com/training/helloboot/service/AccountUserDetailService.java
@@ -2,7 +2,6 @@ package com.training.helloboot.service;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,22 +12,22 @@ import org.springframework.stereotype.Service;
 import com.training.helloboot.repo.AccountRepo;
 
 @Service
-public class AccountUserDetailService implements UserDetailsService{
-	
-	@Autowired
+public class AccountUserDetailService implements UserDetailsService {
+
 	private AccountRepo repo;
+
+	public AccountUserDetailService(AccountRepo repo) {
+		super();
+		this.repo = repo;
+	}
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		return repo.findOneByEmail(email)
-				.map(a -> User.withUsername(a.getEmail())
-							.password(a.getPassword())
-							.authorities(Arrays.asList(new SimpleGrantedAuthority("ROLE_".concat(a.getRole().toString()))))
-							.build())
-				.orElseThrow(() -> new UsernameNotFoundException(String.format("There is no user with this %s address", email)));
-		
+		return repo.findOneByEmail(email).map(a -> User.withUsername(a.getEmail()).password(a.getPassword())
+				.authorities(Arrays.asList(new SimpleGrantedAuthority("ROLE_".concat(a.getRole().toString())))).build())
+				.orElseThrow(() -> new UsernameNotFoundException(
+						String.format("There is no user with this %s address", email)));
+
 	}
-	
-	
 
 }
